a6d6f7c6090423f6a5a42acf5b226fbbb6d14965
相霄 <xiangxiao@xiangxiaodeMacBook-Pro.local>
2019-03-05 19:00:10 +0800

    删除dist文件

--- packages/typescript-jest/server/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@xx-learning-typescript-jest/common");
-exports.serverfn = () => {
-    return common_1.commonfn("world") + "!!";
-};
-console.log(exports.serverfn());
-//# sourceMappingURL=index.js.map
