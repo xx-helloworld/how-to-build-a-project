9eaa92c522626b155c58d3252d3949950920b5e6
System Administrator <root@xiangxiaodeMacBook-Pro.local>
2019-03-16 12:14:48 +0800

    feat: 🎸 新增文档

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -1,50 +1,36 @@
-const path = require("path")
+const path = require("path");
 module.exports = {
-    title: 'How to build a project',
-    description: 'Just playing around',
-    base: "/how-to-build-a-project/",
-    host: "0.0.0.0",
-    themeConfig: {
-        sidebar: 'auto',
-        // locales: {
-        nav: [
-            { text: 'doc', link: '/' }
-        ],
-        sidebar: {
-            '/': [{
-                title: "开个头",
-                collapsable: false,
-                children: [
-                    '/doc/why',
-                    '/doc/workspaces',
-                    '/doc/typescript',
-                    '/doc/typescript-jest'
-                ]
-            },{
-                title: "ui-demo",
-                collapsable: false,
-                children: [
-                    '/uidemo/button'
-                ]
-            }]
+  title: "How to build a project",
+  description: "Just playing around",
+  base: "/how-to-build-a-project/",
+  host: "0.0.0.0",
+  themeConfig: {
+    sidebar: "auto",
+    // locales: {
+    nav: [{ text: "doc", link: "/" }],
+    sidebar: {
+      "/": [
+        {
+          title: "开个头",
+          collapsable: false,
+          children: [
+            "/doc/before",
+            "/doc/why",
+            "/doc/workspaces",
+            "/doc/typescript",
+            "/doc/typescript-jest"
+          ]
+        },
+        {
+          title: "ui-demo",
+          collapsable: false,
+          children: ["/uidemo/button"]
         }
-    },
-    
-    plugins: [   
-        // [
-        //     '@xx-learning/el-button',
-        //     true
-        // ]     
-        [ 
-            '@vuepress/register-components', 
-            {
-              components: [
-                {
-                  name: 'el-button',
-                  path: '../../../../packages/uidemo/button/src/button.vue'
-                }
-              ]
-            }
-        ]
-    ]
-}
+      ]
+    }
+  },
+
+  plugins: [
+    ["@vuepress/back-to-top", true]
+  ]
+};
